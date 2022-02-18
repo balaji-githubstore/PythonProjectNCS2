89e9efe8b50ea15db52581763baf93332c17d102
balaji-githubstore <balaji0017@gmail.com>
2022-02-18 17:33:12 +0530

    datatypes demo

---
+++ demo1_datatypes/datatypes_demo.py
@@ -0,0 +1,30 @@
+name = 'balaji dinakaran'
+
+print(name)
+
+a = 10.2
+
+print(a + a)
+
+print(type(a))
+print(type(name))
+
+b = 20
+print(b)
+print(type(b))
+
+base = 25
+height = 1
+
+area_of_triangle = (base * height) / 2
+print(area_of_triangle)
+
+print(type(area_of_triangle))
+
+check = True
+print(check)
+
+print(type(check))
+
+res = b % 3
+print(res)
