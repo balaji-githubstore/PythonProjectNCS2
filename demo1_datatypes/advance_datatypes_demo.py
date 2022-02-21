6977041b7566eaf0ceeae5b09f378cb6bf927d38
balaji-githubstore <balaji0017@gmail.com>
2022-02-21 12:21:33 +0530

    dictionary & if condition

--- demo1_datatypes/advance_datatypes_demo.py
+++ demo1_datatypes/advance_datatypes_demo.py
@@ -8,7 +8,6 @@ print(colors[0])
 list_size = len(colors)
 print(list_size)
 
-
 colors.append('blue')
 colors.append('white')
 print(colors)
@@ -18,7 +17,7 @@ colors.remove('yellow')
 
 print(colors)
 
-colors.insert(1,'orange')
+colors.insert(1, 'orange')
 print(colors)
 
 colors.append('red')
@@ -29,6 +28,49 @@ print(colors.count('blue'))
 
 print(len(colors))
 
+# will start at 10:55 AM IST
+
+fruits = ['banana', 'mango', 'orange']
+print(fruits)
+# remove the item at index 1 and returns it
+removed_item = fruits.pop(1)
+print(removed_item)
+print(fruits)
+fruits.append('pineapple')
+print(fruits)
+fruits.insert(2, 'JackFruit')
+print(fruits)
+
+signal = ('red', 'green', 'yellow')
+print(signal[0])
+
+appium_dic = {
+    'platformName': 'android',
+    'deviceName': 'redmi k20',
+    'browserName': 'chrome',
+    'deviceId':909983
+}
+
+print(appium_dic)
+print(appium_dic['deviceName'])
+
+
+student_record={
+    'studentId':1001,
+    'studentName':'John',
+    'percentage':72.7,
+    'mailId':'john@gmail.com',
+    'marks':[55,98,56,89,88]
+}
+
+print(student_record['studentId'])
+
+print(student_record['marks'])
+
+print(student_record['marks'][1])
+
+item_count=len(student_record['marks'])
+print(item_count)
 
 
 
---
+++ demo2_logics/logics_demo.py
@@ -0,0 +1,21 @@
+num = 9
+
+if num > 0:
+    print("Positive number")
+    print("*"*50)
+elif num==0:
+    print("It is zero!!!")
+    print("*" * 50)
+else:
+    print("Negative number")
+    print("*" * 50)
+
+browser_name="ch"
+
+# 1.	Declare the browsername and intialize with either ch or ff or edge or ie
+# 2.	Write the if condition to launch the browser name based on the variable declared
+
+# numbers=[98,-99,89]
+#
+# if numbers[2]>0:
+#     print("posiiiiitive")
