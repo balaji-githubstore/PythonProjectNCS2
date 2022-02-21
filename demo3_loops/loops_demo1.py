5877923fe38a95e803b2f0ad495ccf8b0974d163
balaji-githubstore <balaji0017@gmail.com>
2022-02-21 15:25:35 +0530

    loops and list

--- demo3_loops/loops_demo1.py
+++ demo3_loops/loops_demo1.py
@@ -1,6 +1,14 @@
 # print number 1 to 10
 
-for i in range(1, 11):
-    print(i)
+# for i in range(1, 11):
+#     print(i)
 
-numbers = [78, 66, 88, 22, 32, 8, 77, 7, 99, 9]
+numbers = [78, 66, 88, 22, 32, 8, 77, 7, 99, 9,11,30]
+
+size=len(numbers)
+
+print(size)
+
+for num in range(0,len(numbers)):
+    print(numbers[num])
+
